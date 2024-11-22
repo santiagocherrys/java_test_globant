c7d1dc46762b13689ee306242955cf1eb9b39e32
Santiago Echeverry <santiagocherrys@hotmail.com>
2024-11-22 07:48:42 -0500

    Added entity-model and controller customer

---
+++ src/main/java/org/globant/controllers/CustomerController.java
@@ -0,0 +1,49 @@
+package org.globant.controllers;
+
+import org.globant.entity.Customer;
+import org.globant.models.CustomerModel;
+
+import javax.swing.*;
+
+public class CustomerController {
+
+    public static void create(){
+        //Use the model
+        CustomerModel objCustomerModel = new CustomerModel();
+
+        //Ask data to the coder
+        String name = JOptionPane.showInputDialog(("Insert name"));
+        String email = JOptionPane.showInputDialog("Insert email");
+        String phone = JOptionPane.showInputDialog("Insert phone");
+        String address = JOptionPane.showInputDialog("Insert address");
+
+
+        Customer customer = new Customer(name, email, phone, address);
+
+        //Call the method of insertion y save the object that returns in user
+        customer =  objCustomerModel.insert(customer);
+
+        JOptionPane.showMessageDialog(null, customer.toString());
+    }
+
+    public static void getAll(){
+        CustomerModel objMode = new CustomerModel();
+        String listCustomers = "CUSTOMER LIST \n";
+
+        for(Customer customer: objMode.findAll()){
+            listCustomers += customer.toString() + "\n";
+        }
+
+        JOptionPane.showMessageDialog(null, listCustomers);
+    }
+
+    public static String getAllString(){
+        CustomerModel objMode = new CustomerModel();
+        String listCustomers = "CUSTOMER LIST \n";
+
+        for(Customer customer: objMode.findAll()){
+            listCustomers += customer.toString() + "\n";
+        }
+        return listCustomers;
+    }
+}
